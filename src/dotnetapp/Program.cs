2689cea61981b0128f996c4f0690293023aacff5
Michael Simons <msimons@microsoft.com>
2021-04-15 09:05:07 -0500

    Edit

--- src/dotnetapp/Program.cs
+++ src/dotnetapp/Program.cs
@@ -7,6 +7,7 @@ namespace dotnetapp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("------------");
         }
     }
 }
